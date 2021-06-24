8fdbbb277c54897a359a298188561b6b2ee3d309
travisprol0 <travisjohnprol@gmail.com>
2021-06-24 19:58:37 -0400

    model opening and closing

--- src/components/ShowMovie.js
+++ src/components/ShowMovie.js
@@ -1,24 +1,58 @@
 import react, { useEffect, useState } from "react";
+import "../css/ShowMovie.css";
 
 function ShowMovie(props) {
   const [state, setState] = useState([]);
   useEffect(() => {
-    fetch(`https://movie-database-imdb-alternative.p.rapidapi.com/?i=${props.movieID}&r=json`, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "be8fdcd51fmshfe9bee8fdbbb816p1b9c3fjsne461759f5711",
-		"x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com"
-	}
-})
-.then(response => response.json())
-.then(response => {
-	setState(response);
-})
-.catch(err => {
-	console.error(err);
-});
+    fetch(
+      `https://movie-database-imdb-alternative.p.rapidapi.com/?i=${props.movieID}&r=json`,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key":
+            "be8fdcd51fmshfe9bee8fdbbb816p1b9c3fjsne461759f5711",
+          "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        setState(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }, []);
-  return <h1>{state.Title}</h1>;
+  return (
+    <div className="show-movie-container">
+      <div className="show-poster-div">
+        <img src={state.Poster} />
+      </div>
+      <div className="show-movie-info-div">
+        <div className="movie-info-one">
+        <h2>{state.Title}</h2>
+          <h4>Director: {state.Director}</h4>
+          <p>Writer(s): {state.Writer}</p>
+          <p>Cast: {state.Actors}</p>
+          <p>Awards: {state.Awards}</p>
+        </div>
+        <div className="movie-info-two">
+          <p>Rating: {state.Rated}</p>
+          <p>Genre(s): {state.Genre}</p>
+          <p>Runtime: {state.Runtime}</p>
+          <p>Release Date: {state.Released}</p>
+          <p>IMDB Rating: {state.imdbRating}</p>
+        </div>
+        {/* <p>Plot: {state.Plot}</p> */}
+      </div>
+      {/* <p>{state.}</p>
+          <p>{state.}</p>
+          <p>{state.}</p>
+          <p>{state.}</p>
+        <p>{state.}</p> */}
+        <button onClick={() => props.closeWindow()}>Close</button>
+    </div>
+  );
 }
 
 export default ShowMovie;
--- src/components/search/MovieCard.js
+++ src/components/search/MovieCard.js
@@ -1,58 +1,65 @@
-import react, { useState } from "react";
+import React, { useState } from "react";
 import "../../css/MovieCard.css";
-import ShowMovie from '../ShowMovie'
-import Modal from 'react-modal'
+import ShowMovie from "../ShowMovie";
+import Modal from 'react-modal';
 
 const customStyles = {
   content: {
-    top: '50%',
-    left: '50%',
-    right: '40vw',
-    bottom: '-5vw',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
+    top: "50%",
+    left: "50%",
+    right: "40vw",
+    bottom: "-5vw",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
   },
 };
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 function MovieCard(props) {
   let subtitle;
-  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalIsOpen, setIsOpen] = React.useState(false);
 
   function openModal() {
     setIsOpen(true);
   }
 
   function afterOpenModal() {
-    // subtitle.style.color = '#f00';
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
   }
 
   function closeModal() {
     setIsOpen(false);
   }
-  
-    function cardClickHandler(id){
-        console.log(id)
-    }
 
   return (
-    <div className="box" onClick={() => openModal()}>
+    <div className="box" >
       <img className="search-movie-card-poster" src={props.movie.Poster} />
       <div className="search-movie-card-text">
         <p className="movie-search-title">{props.movie.Title}</p>
         <p className="movie-search-year">{props.movie.Year}</p>
-        <button onClick={openModal}>Open Modal</button>
-        <Modal
+      <button onClick={openModal}>Open Modal</button>
+        {/* <button onClick={openModal}>Open Modal</button> */}
+      </div>
+      <Modal
         isOpen={modalIsOpen}
         onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
         style={customStyles}
         contentLabel="Example Modal"
       >
-       <ShowMovie movieID={props.id} />
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
       </Modal>
-      </div>
     </div>
   );
 }
